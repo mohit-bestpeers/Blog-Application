5720887f479ae404cf43656f3f0fe06d5fe42e43
mohit-bestpeers <mobodkhe@bestpeers.com>
2023-06-09 13:51:09 +0530

    Added action mailer feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # before_action :authenticate_user!
 
+  after_create :send_welcome_email
+  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+  def send_welcome_email
+    UserMailer.welcome_email(self).deliver_now
+  end         
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -56,10 +56,26 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Suppress logger output for asset requests.
   config.assets.quiet = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { 
+  :host => '3000', 
+  :protocol => 'localhost'
+  }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            'kuldeeplovanshi777@gmail.com',
+    password:             'dswvlceeryfdpmdi',
+    authentication:       'plain',
+    # enable_starttls_auto: true
+  }
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # Suppress logger output for asset requests.
+
 
 
   # Raises error for missing translations.
